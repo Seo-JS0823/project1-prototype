f1536b03cae520411cfa5a4442b7d80ec0225b50
Seo-JS0823 <seong0093571@naver.com>
2025-08-16 16:26:03 +0900

    proto ver-0.0.3

---
+++ SQL/Industry Table/Insert_Major.sql
@@ -0,0 +1,13 @@
+INSERT ALL
+   into industry_major (major_code, major_name) values ('SEV', '서비스업')
+   into industry_major (major_code, major_name) values ('MED', '의료')
+   into industry_major (major_code, major_name) values ('MAC', '제조/화학')
+   into industry_major (major_code, major_name) values ('DIS', '유통')
+   into industry_major (major_code, major_name) values ('SAL', '판매')
+   into industry_major (major_code, major_name) values ('IWC', 'IT/웹/통신')
+   into industry_major (major_code, major_name) values ('CON', '건설업')
+   into industry_major (major_code, major_name) values ('EDU', '교육업')
+   into industry_major (major_code, major_name) values ('MDG', '미디어/디자인')
+   into industry_major (major_code, major_name) values ('FBK', '은행/금융업')
+   into industry_major (major_code, major_name) values ('ASS', '기관/협회')  
+SELECT * FROM DUAL;
---
+++ SQL/Industry Table/Insert_Sub.sql
@@ -0,0 +1,107 @@
+INSERT ALL
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'A', '호텔/여행/항공')
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'B', '외식업/식음료')
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'C', '웨딩/장례/이벤트')
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'D', 'AS/카센터/주유')
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'E', '뷰티/미용')
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'F', '시설관리/경비/용역')
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'G', '레저/스포츠/여가')
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'H', '렌탈/임대')
+   into industry_sub (major_code, sub_code, sub_name) values ('SEV', 'I', '기타서비스업')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'A', '대학병원')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'B', '종합병원')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'C', '전문병원')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'D', '검진센터')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'E', '정신병원')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'F', '요양병원')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'G', '국공립병원')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'H', '보건소')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MED', 'I', '한방병원')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'A', '전기/전자/제어')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'B', '석유/화학/에너지')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'C', '식품가공/개발')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'D', '반도체/광학/LCD')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'E', '섬유/의류/패션')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'F', '환경')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'G', '농업/어업/광업/임업')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'H', '기계/설비/자동차')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'I', '가구/목재/제지')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'J', '조선/항공/우주')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'K', '화장품/뷰티')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'L', '금속/재료/철강/요업')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'M', '생활용품/소비재/사무')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MAC', 'N', '기타제조업')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('DIS', 'A', '운송')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('DIS', 'B', '수송')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('DIS', 'C', '물류')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('SAL', 'A', '백화점')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('SAL', 'B', '쇼핑몰')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('SAL', 'C', '대형마트')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('SAL', 'D', '편의점')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('SAL', 'E', '홈쇼핑')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'A', '웹 에이전시')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'B', '포털/인터넷/컨텐츠')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'C', '쇼핑몰/오픈마켓')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'D', '네트워크/통신/모바일')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'E', '하드웨어/장비')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'F', '정보보안/백신')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'G', '솔루션/SI/ERP/CRM')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'H', 'IT컨설팅')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('IWC', 'I', '게임')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('CON', 'A', '건설/건축/토목/시공')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('CON', 'B', '실내/인테리어/조경')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('CON', 'C', '환경/설비')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('CON', 'D', '부동산/임대/중개')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('EDU', 'A', '초중고/대학')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('EDU', 'B', '유아/유치원')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('EDU', 'C', '교재/학습지')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('EDU', 'D', '학원/어학원')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('EDU', 'E', '전문/기능학원')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'A', '신문/잡지/언론사')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'B', '방송사/케이블')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'C', '연예/엔터테인먼트')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'D', '광고/홍보/전시')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'E', '영화/배급/음악')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'F', '공연/예술/문화')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'G', '출판/인쇄/사진')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'H', '캐릭터/애니메이션')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('MDG', 'I', '디자인/설계')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('FBK', 'A', '은행/금융/저축')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('FBK', 'B', '대출/캐피탈')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('FBK', 'C', '증권/보험/카드')
+SELECT * FROM DUAL;
+
+INSERT ALL
+   INTO industry_sub (major_code, sub_code, sub_name) values ('ASS', 'A', '정부/공공기관/공기업')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('ASS', 'B', '협회/단체')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('ASS', 'C', '법률/법무/특허')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('ASS', 'D', '세무/회계')
+   INTO industry_sub (major_code, sub_code, sub_name) values ('ASS', 'E', '연구소')
+SELECT * FROM DUAL;
